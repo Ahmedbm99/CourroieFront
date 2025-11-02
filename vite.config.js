0aa167255e777ecbe1bd828f68d825e07c10adbe
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-11-02 03:55:26 +0100

    Fix GitHub Pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/CourroieFront/',
+  base: '/CourroieFront',
   plugins: [react()],
   server: {
     proxy: {
